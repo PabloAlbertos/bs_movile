a9291d48ad81c7e1abb10c15e9b7584a2bd9a715
Pablo Albertos <pabloalbgil@gmail.com>
2017-01-04 11:38:52 +0100

    added tittle xml and java class

---
+++ app/src/main/java/com/example/pabloalbertos/bs_movile/Tittle.java
@@ -0,0 +1,17 @@
+package com.example.pabloalbertos.bs_movile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by pabloalbertos on 04/01/17.
+ */
+
+public class Tittle extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tittle);
+    }
+}
